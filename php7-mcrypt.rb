19395164b343bef51730ca2bf6d92f3cb9ff691e
Guo Xiao <guoxiao08@gmail.com>
2015-07-11 13:14:08 +0800

    remove bottle

--- php7-mcrypt.rb
+++ php7-mcrypt.rb
@@ -9,13 +9,6 @@ class Php7Mcrypt < AbstractPhp7Extension
     sha256 "1d4a768af5c11122fdc6d3417ef08a98302c3abb943ee704282ce1d92537195e"
   end
 
-  bottle do
-    root_url "https://homebrew.bintray.com/bottles-php"
-    sha256 "07609694adb1377336019ef21d08ffba2451283623fb5898256bc56c72690fc5" => :yosemite
-    sha256 "a50a7ed26a8e815d2886da7984bb117db5de86a92c7ca05ecf74374327b96a81" => :mavericks
-    sha256 "2b7b8bf256e6024d954e96b6089d63b61c5ef4e9207a9ce1d4ef9a552d4afa72" => :mountain_lion
-  end
-
   depends_on "mcrypt"
 
   def install
